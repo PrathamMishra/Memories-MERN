4d7919c7b9954b0972046705f588c389047a8d50
Pratham Mishra <mishrapratham30@gmail.com>
2021-07-29 01:59:35 +0530

    deploying this on heroku

---
+++ controllers/posts.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js"
+
+export const getPosts = async (req,res)=>{
+    try{
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+        res.status(200).json(postMessages);
+    }catch(err){
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const createPost = async (req,res) => {
+    const post=req.body;
+    const newPost= new PostMessage(post);
+    try{
+        await newPost.save();
+        res.status(201).json(newPost);
+    }catch(err){
+        res.status(409).json({ message: err.message });
+    }
+};
+
+export const updatePost = async (req,res) => {
+    const { id: _id}=req.params;
+    const post = req.body;
+    if(!mongoose.Types.ObjectId.isValid(_id))return res.status(404).send("No Post with that id.")
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id } , {new : true});
+    res.json(updatedPost);
+};
+
+export const deletePost = async (req,res) => {
+    const { id}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))return res.status(404).send("No Post with that id.")
+
+    await PostMessage.findByIdAndDelete(id);
+    res.json({message: "Post deleted successfully."});
+};
+
+export const likePost = async (req,res) => {
+    const { id }=req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))return res.status(404).send("No Post with that id.")
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount+1 }, {new: true});
+    res.json(updatedPost);
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+
+dotenv.config();
+const app=express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts",postRoutes);
+
+app.get("/", (req,res) => {
+    res.send("Hello to memories API");
+})
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        app.listen(PORT,console.log(`Server running on ${PORT}`));
+    })
+    .catch((err)=>{
+        console.log(err.message);
+    });
+
+mongoose.set("useFindAndModify",false);
